5d861f97670a3f0013405578f3cd7ab705d1ab8a
Freek Van der Herten <freek@spatie.be>
2017-12-21 16:27:37 +0000

    Apply fixes from StyleCI

--- tests/scripts/non-auto-destruct.php
+++ tests/scripts/non-auto-destruct.php
@@ -4,7 +4,7 @@ require '../../src/TemporaryDirectory';
 
 use Spatie\TemporaryDirectory\TemporaryDirectory;
 
-$path = $testingDirectory = __DIR__.DIRECTORY_SEPARATOR . '..' .DIRECTORY_SEPARATOR .'temp';
+$path = $testingDirectory = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'temp';
 
 $temporaryDirectory = new TemporaryDirectory($path);
 
