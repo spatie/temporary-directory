0d599fc638b88ce5938f58a866f7d4aa65b088f7
Patrick <trick.developer@gmail.com>
2021-03-30 14:29:41 -0400

    fix syntax error

--- src/Exceptions/InvalidDirectoryName.php
+++ src/Exceptions/InvalidDirectoryName.php
@@ -6,6 +6,6 @@ class InvalidDirectoryName extends \Exception
 {
     public static function create(string $directoryName): static
     {
-        return new static("The directory name `{$name}` contains invalid characters.");
+        return new static("The directory name `{$directoryName}` contains invalid characters.");
     }
 }
