fd33f25b29e7e6e798647b08c32bd1dd77035578
Emeka Ndefo <ndefocn@yahoo.com>
2023-04-28 03:19:55 +0100

    Added try catch
    Solved error: rmDir() Directory not empty, to return a success message ("Backup completed!")
    Tested with Laravel 10

--- src/TemporaryDirectory.php
+++ src/TemporaryDirectory.php
@@ -151,30 +151,38 @@ class TemporaryDirectory
 
     protected function deleteDirectory(string $path): bool
     {
-        if (is_link($path)) {
-            return unlink($path);
-        }
+        $res = false;
 
-        if (! file_exists($path)) {
-            return true;
-        }
-
-        if (! is_dir($path)) {
-            return unlink($path);
-        }
-
-        foreach (new FilesystemIterator($path) as $item) {
-            if (! $this->deleteDirectory($item)) {
-                return false;
+        try {
+            if (is_link($path)) {
+                return unlink($path);
             }
+
+            if (! file_exists($path)) {
+                return true;
+            }
+
+            if (! is_dir($path)) {
+                return unlink($path);
+            }
+
+            foreach (new FilesystemIterator($path) as $item) {
+                if (! $this->deleteDirectory($item)) {
+                    return false;
+                }
+            }
+
+            /*
+             * By forcing a php garbage collection cycle using gc_collect_cycles() we can ensure
+             * that the rmdir does not fail due to files still being reserved in memory.
+             */
+            gc_collect_cycles();
+
+            $res = rmdir($path);
+        } catch (\Throwable $th) {
+            //throw $th;
         }
 
-        /*
-         * By forcing a php garbage collection cycle using gc_collect_cycles() we can ensure
-         * that the rmdir does not fail due to files still being reserved in memory.
-         */
-        gc_collect_cycles();
-
-        return rmdir($path);
+        return $res;
     }
 }
