b3c6a4918b92b090a1b711d8556bdcbb31c4cc5c
patrik <patrik@silentridge.io>
2023-12-20 22:01:20 +0100

    minor refactoring to use callbacks from a more appropriate place

--- src/TemporaryDirectory.php
+++ src/TemporaryDirectory.php
@@ -17,12 +17,18 @@ class TemporaryDirectory
     protected string $name = '';
     protected bool $useLifecycleEvents = false;
     protected bool $forceCreate = false;
-
     protected bool $deleteWhenDestroyed = false;
 
     public function __construct(string $location = '')
     {
         $this->location = $this->sanitizePath($location);
+    }
+
+    protected function sanitizePath(string $path): string
+    {
+        $path = rtrim($path);
+
+        return rtrim($path, DIRECTORY_SEPARATOR);
     }
 
     public static function make(string $location = ''): self
@@ -53,6 +59,98 @@ class TemporaryDirectory
         return $this;
     }
 
+    protected function getSystemTemporaryDirectory(): string
+    {
+        return rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR);
+    }
+
+    protected function getFullPath(): string
+    {
+        return $this->location . (!empty($this->name) ? DIRECTORY_SEPARATOR . $this->name : '');
+    }
+
+    protected function deleteDirectory(string $path): bool
+    {
+        try {
+            if (is_link($path)) {
+                $result = unlink($path);
+                return $result;
+            }
+
+            if (!file_exists($path)) {
+                return true;
+            }
+
+            if (!is_dir($path)) {
+                $result = unlink($path);
+                return $result;
+            }
+
+            foreach (new FilesystemIterator($path) as $item) {
+                if (!$this->deleteDirectory($item)) {
+                    return false;
+                }
+            }
+
+            /*
+             * By forcing a php garbage collection cycle using gc_collect_cycles() we can ensure
+             * that the rmdir does not fail due to files still being reserved in memory.
+             */
+            gc_collect_cycles();
+
+            return $this->rmdir($path);
+
+
+        } catch (Throwable) {
+            return false;
+        }
+    }
+
+    public function rmdir($path)
+    {
+        if (file_exists($path) === false || is_dir($path) === false) {
+            throw new DirectoryNotFoundException("Directory $path not found");
+        }
+        if ($this->useLifecycleEvents) {
+            $this->callBeforeDeleteCallbacks();
+        }
+        if (false === rmdir($path)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not deleted', $path));
+        }
+        if ($this->useLifecycleEvents) {
+            $this->callAfterDeleteCallbacks();
+        }
+        return true;
+    }
+
+    public function exists(): bool
+    {
+        return file_exists($this->getFullPath());
+    }
+
+    public function mkdir($directoryPath, $mode = 0777, $recursive = true)
+    {
+        if (!file_exists($directoryPath)) {
+            if ($this->useLifecycleEvents) {
+                $this->callBeforeCreateCallbacks();
+            }
+            if (!mkdir($directoryPath, 0777, true) && !is_dir($directoryPath)) {
+                throw new RuntimeException(sprintf('Directory "%s" was not created', $directoryPath));
+            }
+            if ($this->useLifecycleEvents) {
+                $this->callAfterCreateCallbacks();
+            }
+        } else {
+            throw new DirectoryAlreadyExistsException("Directory $directoryPath already exists");
+        }
+    }
+
+    public function useLifecycleEvents(bool $useLifecycleEvents = true): TemporaryDirectory
+    {
+        $this->useLifecycleEvents = $useLifecycleEvents;
+        return $this;
+    }
+
     public function force(): self
     {
         $this->forceCreate = true;
@@ -65,6 +163,20 @@ class TemporaryDirectory
         $this->name = $this->sanitizeName($name);
 
         return $this;
+    }
+
+    protected function sanitizeName(string $name): string
+    {
+        if (!$this->isValidDirectoryName($name)) {
+            throw InvalidDirectoryName::create($name);
+        }
+
+        return trim($name);
+    }
+
+    protected function isValidDirectoryName(string $directoryName): bool
+    {
+        return strpbrk($directoryName, '\\/?%*:|"<>') === false;
     }
 
     public function location(string $location): self
@@ -91,72 +203,6 @@ class TemporaryDirectory
         return $path;
     }
 
-    public function empty(): self
-    {
-        $this->deleteDirectory($this->getFullPath());
-
-        $this->mkdir($this->getFullPath(), 0777, true);
-
-        return $this;
-    }
-
-    public function mkdir($directoryPath, $mode = 0777, $recursive = true)
-    {
-        if (!file_exists($directoryPath)) {
-            if ($this->useLifecycleEvents) {
-                $this->callBeforeCreateCallbacks();
-            }
-            if (!mkdir($directoryPath, 0777, true) && !is_dir($directoryPath)) {
-                throw new RuntimeException(sprintf('Directory "%s" was not created', $directoryPath));
-            }
-            if ($this->useLifecycleEvents) {
-                $this->callAfterCreateCallbacks();
-            }
-        }
-
-    }
-
-    public function delete(): bool
-    {
-        return $this->deleteDirectory($this->getFullPath());
-    }
-
-    public function exists(): bool
-    {
-        return file_exists($this->getFullPath());
-    }
-
-    protected function getFullPath(): string
-    {
-        return $this->location . (!empty($this->name) ? DIRECTORY_SEPARATOR . $this->name : '');
-    }
-
-    protected function isValidDirectoryName(string $directoryName): bool
-    {
-        return strpbrk($directoryName, '\\/?%*:|"<>') === false;
-    }
-
-    protected function getSystemTemporaryDirectory(): string
-    {
-        return rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR);
-    }
-
-    protected function sanitizePath(string $path): string
-    {
-        $path = rtrim($path);
-
-        return rtrim($path, DIRECTORY_SEPARATOR);
-    }
-
-    protected function sanitizeName(string $name): string
-    {
-        if (!$this->isValidDirectoryName($name)) {
-            throw InvalidDirectoryName::create($name);
-        }
-
-        return trim($name);
-    }
-
     protected function removeFilenameFromPath(string $path): string
     {
         if (!$this->isFilePath($path)) {
@@ -171,58 +217,13 @@ class TemporaryDirectory
         return str_contains($path, '.');
     }
 
-    protected function deleteDirectory(string $path): bool
+    public function empty(): self
     {
-        try {
-            if (is_link($path)) {
-                if ($this->useLifecycleEvents) {
-                    $this->callBeforeDeleteCallbacks();
-                }
-                $result= unlink($path);
-                if ($this->useLifecycleEvents) {
-                    $this->callAfterDeleteCallbacks();
-                }
-                return $result;
-            }
+        $this->deleteDirectory($this->getFullPath());
 
-            if (!file_exists($path)) {
-                return true;
-            }
+        $this->mkdir($this->getFullPath(), 0777, true);
 
-            if (!is_dir($path)) {
-                if ($this->useLifecycleEvents) {
-                    $this->callBeforeDeleteCallbacks();
-                }
-                $result= unlink($path);
-                if ($this->useLifecycleEvents) {
-                    $this->callAfterDeleteCallbacks();
-                }
-
-                return $result;
-            }
-
-            foreach (new FilesystemIterator($path) as $item) {
-                if (!$this->deleteDirectory($item)) {
-                    return false;
-                }
-            }
-
-            /*
-             * By forcing a php garbage collection cycle using gc_collect_cycles() we can ensure
-             * that the rmdir does not fail due to files still being reserved in memory.
-             */
-            gc_collect_cycles();
-            if ($this->useLifecycleEvents) {
-                $this->callBeforeDeleteCallbacks();
-            }
-            $result= rmdir($path);
-            if ($this->useLifecycleEvents) {
-                $this->callAfterDeleteCallbacks();
-            }
-            return $result;
-        } catch (Throwable) {
-            return false;
-        }
+        return $this;
     }
 
     public function deleteWhenDestroyed(bool $deleteWhenDestroyed = true): self
@@ -237,5 +238,10 @@ class TemporaryDirectory
         if ($this->deleteWhenDestroyed) {
             $this->delete();
         }
+    }
+
+    public function delete(): bool
+    {
+        return $this->deleteDirectory($this->getFullPath());
     }
 }
